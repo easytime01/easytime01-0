b820fee627061a17338352e6dd8b0b65f6fd5131
Peter <Peter@192.168.1.62>
2015-05-20 22:25:38 +0200

    second

---
+++ EasyTime/src/fr/easytime/database/DatabaseHelper.java
@@ -0,0 +1,29 @@
+package fr.easytime.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+  private static final String DATABASE_NAME = "easytimetable.db";
+  private static final int DATABASE_VERSION = 1;
+
+  public DatabaseHelper(Context context) {
+    super(context, DATABASE_NAME, null, DATABASE_VERSION);
+  }
+
+  // Méthode appelée pendant la création de la base de données
+  @Override
+  public void onCreate(SQLiteDatabase database) {
+    Table.onCreate(database);
+  }
+
+  // Méthode appelée pendant une mise a jour de la base de  
+  // données, par exemple vous augmentez sa version 
+  @Override
+  public void onUpgrade(SQLiteDatabase database, int oldVersion,
+      int newVersion) {
+    Table.onUpgrade(database, oldVersion, newVersion);
+  }
+}
