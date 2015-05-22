852eea247908bf4baec7cec4c3b7ddb9214c7642
Peter <Peter@192.168.1.24>
2015-05-22 21:42:57 +0200

    22 mai : connection en cours

--- EasyTime/src/fr/easytime/ConnectActivity.java
+++ EasyTime/src/fr/easytime/ConnectActivity.java
@@ -1,15 +1,33 @@
 package fr.easytime;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class ConnectActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_connect);
+        
+        final Button loginButton = (Button) findViewById(R.id.button1);
+        final Button editText1 = (Button) findViewById(R.id.editText1);
+        final Button editText2 = (Button) findViewById(R.id.editText2);
+        
+        loginButton.setOnClickListener(new OnClickListener() {
+      			
+        @Override
+        public void onClick(View v) {  
+             
+        	
+        	
+      	}
+      });
     }
 }
